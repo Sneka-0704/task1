cde1042d0445deab35466e919a42200a2e13b2e6
Sneka-0704 <snekavarshini07@gmail.com>
2024-01-21 21:56:20 +0530

    Alpha

---
+++ p1.js
@@ -0,0 +1,12 @@
+a='a'
+console.log("1.",a)
+var b='var'
+console.log("this variable belongs to var",b)
+var b=10;
+console.log("this variable belongs to var",b)
+let c='let'
+console.log("this variable belongs to let c='let' var",c)
+const d=100
+console.log("constant variable belongs to var",d)
+//d=200
+//console.log(d)
